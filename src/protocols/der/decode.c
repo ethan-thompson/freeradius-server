26b879312591b65cc55add7ab4587a8359b2aafa
ethan-thompson <ethan.thompson@networkradius.com>
2025-01-15 13:19:11 -0500

    fix: Fixed problem where DEFAULT values would not be used if there was no more data left to decode (such as the case with the final value in a sequence.)
    Signed-off-by: ethan-thompson <ethan.thompson@networkradius.com>

--- src/protocols/der/decode.c
+++ src/protocols/der/decode.c
@@ -2631,12 +2631,45 @@ static ssize_t fr_der_decode_pair_dbuff(TALLOC_CTX *ctx, fr_pair_list_t *out, fr
 	 *
 	 */
 
+	/*
+	 *	If there are no more bytes to read, it is possible that the value was not encoded since
+	 *	it may be using the DEFAULT value
+	 */
+	if ((fr_dbuff_remaining(&our_in) == 0) && fr_der_flag_has_default(parent)) {
+		fr_pair_t	     *vp = fr_pair_afrom_da(ctx, parent);
+		fr_dict_enum_value_t *ev;
+
+		if (unlikely(vp == NULL)) {
+			fr_strerror_const("Out of memory for pair");
+			return -1;
+		}
+
+		ev = fr_dict_enum_by_name(parent, "DEFAULT", strlen("DEFAULT"));
+		if (unlikely(ev == NULL)) {
+			fr_strerror_printf("No DEFAULT value for attribute %s", parent->name);
+			return -1;
+		}
+
+		if (fr_value_box_copy(vp, &vp->data, ev->value) < 0) return -1;
+
+		vp->data.enumv = vp->da;
+
+		fr_pair_append(out, vp);
+
+		return 0;
+	}
+
 	if (unlikely(fr_der_decode_hdr(parent, &our_in, &tag, &len) < 0)) return -1;
 
 	FR_PROTO_TRACE("Attribute %s, tag %" PRIu64, parent->name, tag);
 
 	if (!fr_type_to_der_tag_valid(parent->type, tag)) {
 		if (fr_der_flag_has_default(parent)) {
+			/*
+			 *	If the attribute has a default value, we will use that.
+			 *	We could end up here if we are decoding a sequence which has a default value
+			 *	for an attribute that is not the last attribute in the sequence.
+			 */
 			fr_pair_t	     *vp = fr_pair_afrom_da(ctx, parent);
 			fr_dict_enum_value_t *ev;
 
