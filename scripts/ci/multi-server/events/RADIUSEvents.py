9a31514a9f656501ab9261a0b703111c1be63af4
ethan-thompson <ethan.thompson@networkradius.com>
2025-09-22 16:28:25 +0000

    feat: Added basic radius event to send an access-request to a container
    Signed-off-by: ethan-thompson <ethan.thompson@networkradius.com>

---
+++ scripts/ci/multi-server/events/RADIUSEvents.py
@@ -0,0 +1,27 @@
+"""Module for RADIUS-related events in a multi-server CI environment."""
+
+from typing import Union
+from python_on_whales import Container, docker
+
+ValidContainer = Union[Container, str]
+
+def access_request(
+    source: ValidContainer,
+    target: ValidContainer,
+    secret: str,
+    username: str,
+    password: str,
+) -> None:
+    """
+    Simulate a RADIUS access request.
+
+    Args:
+        container (ValidContainer): The container to execute the command in.
+        command (str): The command to execute.
+
+    Raises:
+        python_on_whales.exceptions.NoSuchContainer: If the container does not exist.
+    """
+    command = f"echo {password} | radtest {username} {password} {target} 0 {secret} || true"
+
+    docker.execute(source, ["bash", "-c", command], detach=True)
