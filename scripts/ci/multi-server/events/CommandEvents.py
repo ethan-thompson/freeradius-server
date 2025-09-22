e121994f5833f215dc7600fd4e1d2dd9cee179e2
ethan-thompson <ethan.thompson@networkradius.com>
2025-09-22 16:29:48 +0000

    feat: Added a generic command event that will allow arbitrary commands to be run in a target container.
    Signed-off-by: ethan-thompson <ethan.thompson@networkradius.com>

---
+++ scripts/ci/multi-server/events/CommandEvents.py
@@ -0,0 +1,25 @@
+"""Module for handling command events in a multi-server test environment."""
+
+from typing import Union
+from python_on_whales import Container, docker
+
+ValidContainer = Union[Container, str]
+
+
+def run_command(
+    container: ValidContainer,
+    command: str,
+    detach: bool = False,
+) -> None:
+    """
+    Execute a command in a specified container.
+
+    Args:
+        container (ValidContainer): The container to execute the command in.
+        command (str): The command to execute.
+        detach (bool, optional): Whether to run the command in detached mode. Defaults to False.
+
+    Raises:
+        python_on_whales.exceptions.NoSuchContainer: If the container does not exist.
+    """
+    docker.execute(container, ["bash", "-c", command], detach=detach)
